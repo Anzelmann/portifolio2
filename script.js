7e9210c0a55b84f37e04ec2db84f2d6fcac988d1
Anzelmann <160858152+Anzelmann@users.noreply.github.com>
2025-03-18 00:19:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function inicio() {
+
+  location = "index.html";
+  
+}
+
+function carreira() {
+
+  location = "carreira.html";
+  
+}
+
+function projetos() {
+
+  location = "projetos.html";
+  
+}
+
+function sobre() {
+
+  location = "sobre.html";
+  
+}
